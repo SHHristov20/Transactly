a91a1373f463a45644e8a24ec7edc32f92181c53
Stefan Hristov <SHHristov20@codingburgas.bg>
2024-11-26 15:51:20 +0200

    Create initial database

---
+++ Transactly/Transactly.Data/Data/Contexts/TransactlyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Transactly.Data.Data.Contexts
+{
+    public class TransactlyDbContext : DbContext
+    {
+
+
+        public TransactlyDbContext() { }
+        public TransactlyDbContext(DbContextOptions<TransactlyDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Transactly;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
