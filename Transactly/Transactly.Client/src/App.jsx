3512e1395d9100aa5485ed3c4b2699426d80be2f
Maxim Marinov <MMMarinov20@codingburgas.bg>
2024-11-26 15:05:04 +0200

    Create sample register page

--- Transactly/Transactly.Client/src/App.jsx
+++ Transactly/Transactly.Client/src/App.jsx
@@ -1,35 +1,33 @@
 import React from "react";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { FaSearch } from "react-icons/fa";
-import { CiSettings } from "react-icons/ci";
-import { IoIosNotifications } from "react-icons/io";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <React.Fragment>
-      <div className="w-screen flex justify-between p-5">
-        <div className="flex justify-between items-center w-screen">
-          <RxHamburgerMenu className="md:hidden text-2xl" />
-          <h1 className="text-3xl md:text-4xl">Home</h1>
-          <div className="md:hidden w-10 h-10 cursor-pointer bg-purple-500 rounded-full"></div>
-        </div>
-        <div className="space-x-5 hidden md:flex">
-          <div className="flex items-center bg-[#F5F7FA] py-2 px-5 rounded-full">
-            <FaSearch className="text-[#8BA3CB]" />
-            <input
-              type="text"
-              placeholder="Search for something"
-              className="px-2 focus:outline-none bg-[#F5F7FA] placeholder:text-[#8BA3CB]"
-            />
-          </div>
-          <div className="bg-[#F5F7FA] cursor-pointer rounded-full flex items-center justify-center w-12 h-12">
-            <CiSettings className="text-2xl" />
-          </div>
-          <div className="bg-[#F5F7FA] cursor-pointer rounded-full flex items-center justify-center w-12 h-12">
-            <IoIosNotifications className="text-2xl text-[#FE5C73]" />
-          </div>
+      {/* <Navbar /> */}
+      <div className="flex w-screen h-screen justify-center items-center">
+        <div className="flex flex-col gap-y-6">
+          <h1 className="text-center text-4xl font-bold pb-5">Register</h1>
+          <input
+            placeholder="First Name"
+            className="border-b-[1px] focus:outline-none"
+          />
+          <input
+            placeholder="Last Name"
+            className="border-b-[1px] focus:outline-none"
+          />
+          <input
+            placeholder="Phone"
+            className="border-b-[1px] focus:outline-none"
+          />
+          <input
+            placeholder="Password"
+            className="border-b-[1px] focus:outline-none"
+          />
 
-          <div className="w-12 h-12 cursor-pointer bg-purple-500 rounded-full"></div>
+          <button className="bg-pink-600 text-white rounded-full w-full py-3 my-2">
+            Continue
+          </button>
         </div>
       </div>
     </React.Fragment>
---
+++ Transactly/Transactly.Client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaSearch } from "react-icons/fa";
+import { CiSettings } from "react-icons/ci";
+import { IoIosNotifications } from "react-icons/io";
+
+const Navbar = () => {
+  return (
+    <React.Fragment>
+      <div className="w-screen flex justify-between p-5">
+        <div className="flex justify-between items-center w-screen">
+          <RxHamburgerMenu className="md:hidden text-2xl" />
+          <h1 className="text-3xl md:text-4xl">Home</h1>
+          <div className="md:hidden w-10 h-10 cursor-pointer bg-purple-500 rounded-full"></div>
+        </div>
+        <div className="space-x-5 hidden md:flex">
+          <div className="flex items-center bg-[#F5F7FA] py-2 px-5 rounded-full">
+            <FaSearch className="text-[#8BA3CB]" />
+            <input
+              type="text"
+              placeholder="Search for something"
+              className="px-2 focus:outline-none bg-[#F5F7FA] placeholder:text-[#8BA3CB]"
+            />
+          </div>
+          <div className="bg-[#F5F7FA] cursor-pointer rounded-full flex items-center justify-center w-12 h-12">
+            <CiSettings className="text-2xl" />
+          </div>
+          <div className="bg-[#F5F7FA] cursor-pointer rounded-full flex items-center justify-center w-12 h-12">
+            <IoIosNotifications className="text-2xl text-[#FE5C73]" />
+          </div>
+          <div className="w-12 h-12 cursor-pointer bg-purple-500 rounded-full"></div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Navbar;
