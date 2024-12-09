717735e8a51023432d49dd1184976fc4ca3ac618
Maxim Marinov <MMMarinov20@codingburgas.bg>
2024-12-09 20:25:18 +0200

    Implement frontend routing

--- Transactly/Transactly.Client/src/App.jsx
+++ Transactly/Transactly.Client/src/App.jsx
@@ -1,35 +1,16 @@
 import React from "react";
-import Navbar from "./Components/Navbar/Navbar";
+// import Navbar from "./Components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Register from "./Pages/Register";
 
 function App() {
   return (
     <React.Fragment>
       {/* <Navbar /> */}
-      <div className="flex w-screen h-screen justify-center items-center">
-        <div className="flex flex-col gap-y-6 md:w-1/2 xl:w-1/3">
-          <h1 className="text-center text-4xl font-bold pb-5">Register</h1>
-          <input
-            placeholder="First Name"
-            className="border-b-[1px] focus:outline-none"
-          />
-          <input
-            placeholder="Last Name"
-            className="border-b-[1px] focus:outline-none"
-          />
-          <input
-            placeholder="Phone"
-            className="border-b-[1px] focus:outline-none"
-          />
-          <input
-            placeholder="Password"
-            className="border-b-[1px] focus:outline-none"
-          />
-
-          <button className="bg-pink-600 text-white rounded-full w-full py-3 my-2">
-            Continue
-          </button>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
     </React.Fragment>
   );
 }
---
+++ Transactly/Transactly.Client/src/Pages/Register.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <React.Fragment>
+      <div className="flex w-screen h-screen justify-center items-center">
+        <div className="flex flex-col gap-y-6 md:w-1/2 xl:w-1/3">
+          <h1 className="text-center text-4xl font-bold pb-5">Register</h1>
+          <input
+            placeholder="First Name"
+            className="border-b-[1px] focus:outline-none"
+          />
+          <input
+            placeholder="Last Name"
+            className="border-b-[1px] focus:outline-none"
+          />
+          <input
+            placeholder="Phone"
+            className="border-b-[1px] focus:outline-none"
+          />
+          <input
+            placeholder="Password"
+            className="border-b-[1px] focus:outline-none"
+          />
+
+          <button className="bg-pink-600 text-white rounded-full w-full py-3 my-2">
+            Continue
+          </button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Register;
