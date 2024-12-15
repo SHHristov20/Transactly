03f8556856f7ab7736130a6656f624185200f162
Stefan Hristov <SHHristov20@codingburgas.bg>
2024-12-15 21:57:34 +0200

    Implement to method to get all currencies

---
+++ Transactly/Transactly.Server/Controllers/CurrencyController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Transactly.Core.Interfaces;
+using Transactly.Core.DTOs;
+using Transactly.Data.Models;
+using Transactly.Core.Validators;
+using Transactly.Core.Services;
+
+namespace Transactly.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class CurrencyController(ICurrencyService currencyService) : ControllerBase
+    {
+        private readonly ICurrencyService _currencyService = currencyService;
+
+        [HttpGet(Name = "GetAllCurrencies")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _currencyService.GetAll<Currency>());
+        }
+    }
+}
